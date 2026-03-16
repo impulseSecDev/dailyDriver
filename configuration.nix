0f7d58e03469bfe246cb45d64dad7cee0c2712f7
Tim <timothy.harris0187@proton.me>
2026-03-16 12:32:34 -0400

    test

--- configuration.nix
+++ configuration.nix
@@ -36,6 +36,11 @@
       canTouchEfiVariables = true;
       efiSysMountPoint = "/boot"; # ← use the same mount point here.
     };
+    grub = {
+      efiSupport = true;
+      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+      device = "nodev";
+    };
   };
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
