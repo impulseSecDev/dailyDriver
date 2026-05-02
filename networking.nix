7b016ba192448cad010c925baefad3bcc32a697f
Tim <timothy.harris0187@proton.me>
2026-05-01 21:44:12 -0400

    updated acme options due to nix update

--- networking.nix
+++ networking.nix
@@ -11,16 +11,16 @@
     "headscale_ssh_key" = {};
   };
 
-   networking = {
-     interfaces.enp1s0 = {
-       ipv4.addresses = [{
-         address = "192.168.1.153";
-         prefixLength = 24;
-       }];
-     };
-     defaultGateway = "192.168.1.1";
-     nameservers = [ "1.1.1.1" "8.8.8.8" ];
-   };
+  networking = {
+    interfaces.enp1s0 = {
+      ipv4.addresses = [{
+        address = "192.168.1.153";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = "192.168.1.1";
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  };
 
   # Enable the OpenSSH daemon.
   services.openssh = {
